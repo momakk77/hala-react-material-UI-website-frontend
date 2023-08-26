befafd6af1e6ca554448dde171ca2ef07315d005
Tarek Helwani <tarek.helwani1998@gmail.com>
2023-08-26 11:25:56 +0300

    initial load

--- src/components/PhotoExpandDialog.jsx
+++ src/components/PhotoExpandDialog.jsx
@@ -97,7 +97,7 @@ const config = {
           }}
       >
         
-         <Img src={getImage.imagePath} alt=""/>
+         <Img src={getImage.imagePath.replace('/photo' , '')} alt=""/>
 
       </BootstrapDialog>
     </>
