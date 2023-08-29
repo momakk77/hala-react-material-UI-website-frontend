ac4037c95d5a85f7fa8430bc95d5fba0b7ffc47b
Tarek Helwani <tarek.helwani1998@gmail.com>
2023-08-29 16:09:42 +0300

    initial load

--- src/components/PhotoExpandDialog.jsx
+++ src/components/PhotoExpandDialog.jsx
@@ -11,7 +11,7 @@ const Img = styled("img")((props) => ({
   display: "block",
   objectFit: "contain",
   width: "100%",
-  height: "800px",
+  height: "100%",
   }));
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
