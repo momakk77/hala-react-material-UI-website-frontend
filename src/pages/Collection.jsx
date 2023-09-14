019e13808e92d2ee48e36a6a82c7ae91c04c9e8e
Tarek Helwani <tarek.helwani1998@gmail.com>
2023-09-14 15:55:10 +0300

    initial load

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -18,8 +18,8 @@ const Img = styled("img")({
 const Collection = () => {
   const [getAllImages, setGetAllImages] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0);
-  const [getAllCategories, setGetAllCategories] = useState([1]);
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(1);
+  const [getAllCategories, setGetAllCategories] = useState([]);
   const [selectedPage, setSelectedPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [selectedLimit, setSelectedLimit] = useState(8);
